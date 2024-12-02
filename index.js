d559e325fa19956ed8a868a5aa19eaccea9cd0b6
jnewell-7 <jacksonnewell07@gmail.com>
2024-12-02 14:33:46 -0700

    update password

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const knex = require("knex")({
   connection: {
     host: process.env.RDS_HOSTNAME || "localhost",
     user: process.env.RDS_USERNAME || "postgres",
-    password: process.env.RDS_PASSWORD || "admin",
+    password: process.env.RDS_PASSWORD || "I3atorang3s!",
     database: process.env.RDS_DB_NAME || "intex",
     port: process.env.RDS_PORT || 5434,
     ssl: process.env.DB_INTEX ? { rejectUnauthorized: false } : false,
