b3464f611ac02b6dc50976d1206e1fb383f72eb2
jnewell-7 <jacksonnewell07@gmail.com>
2024-12-02 11:20:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let express = require("express");
+let app = express();
+let path = require("path");
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.RDS_HOSTNAME || "localhost",
+    user: process.env.RDS_USERNAME || "postgres",
+    password: process.env.RDS_PASSWORD || "admin",
+    database: process.env.RDS_DB_NAME || "INTEX",
+    port: process.env.RDS_PORT || 5434,
+    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+  },
+});
