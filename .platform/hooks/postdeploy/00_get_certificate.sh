27222c18fd23a321b11ade5b17d2030d580d4018
jnewell-7 <jacksonnewell07@gmail.com>
2024-12-02 17:22:32 -0700

    update domain name

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -2,7 +2,7 @@
 # Place in .platform/hooks/postdeploy directory
 
 # Define your domains
-DOMAINS="-d myturtleshelter.us-east-1.elasticbeanstalk.com"
+DOMAINS="-d myturtleshelter.us-east-1.elasticbeanstalk.com -d trtleproject.is404.net"
 
 # Run Certbot to obtain or renew certificates for both domains
 sudo certbot -n $DOMAINS --nginx --agree-tos --email jnewell7@byu.edu --expand --redirect
