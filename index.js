f3606a1d9317b5a6eca3131f7d10ec6c4fbf88bc
peterhamilton33 <hamiltonpeter02@gmail.com>
2024-12-03 14:00:25 -0700

    login.ejs

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use(express.static(path.join(__dirname, "public")));
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: process.env.RDS_HOSTNAME || "localhost",
+    host: process.env.RDS_HOSTNAME || "awseb-e-fzr58sdsxd-stack-awsebrdsdatabase-2vdqecpsp679.chiykskmafi4.us-east-1.rds.amazonaws.com",
     user: process.env.RDS_USERNAME || "postgres",
-    password: process.env.RDS_PASSWORD || "admin",
+    password: process.env.RDS_PASSWORD || "gocougs123",
     database: process.env.RDS_DB_NAME || "intex",
     port: process.env.RDS_PORT || 5432,
     ssl: process.env.DB_INTEX ? { rejectUnauthorized: false } : false,
@@ -32,6 +32,32 @@ const knex = require("knex")({
 
 
 // Routes
+
+
+//Login Route
+
+app.get('/login', (req, res) => {
+  knex('admin')
+      .select('username', 'password')
+      .then(adminCredentials => {
+          res.render('login', { adminCredentials });
+      })
+      .catch(error => {
+          console.error("Error fetching admin credentials: ", error);
+          res.status(500).send('Internal Server Error');
+      });
+});
+
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  
+  if (username === adminCredentials.username && password === adminCredentials.password) {
+    res.render('admin'); // Render the admin page if credentials are correct
+  } else {
+    res.send('Invalid username or password');
+  }
+});
 
 // Landing Page Get Route
 app.get("/", (req, res) => {
