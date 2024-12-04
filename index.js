413e734f2e8e9f0d6f5eb10212e3b73ebbd43f61
jnewell-7 <jacksonnewell07@gmail.com>
2024-12-03 19:14:12 -0700

    update links

--- index.js
+++ index.js
@@ -29,9 +29,6 @@ const knex = require("knex")({
   },
 });
 
-knex.raw("SELECT 1")
-  .then(() => console.log("Database connection successful"))
-  .catch((err) => console.error("Database connection error:", err));
 // Routes
 
 // Login Page Route (GET)
