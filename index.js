2977f1b57e25ccb90708931785411513ba0fe153
peterhamilton33 <hamiltonpeter02@gmail.com>
2024-12-04 10:12:09 -0700

    volunteer.ejs made

--- index.js
+++ index.js
@@ -141,6 +141,9 @@ app.get("/realDonate", (req, res) => {
   );
 });
 
+app.get("/get-involved", (req, res) => {
+  res.render("volunteer", { title: "Volunteer Today" });
+});
 
 
 // Error Handling Middleware
