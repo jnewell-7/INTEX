7e31f34d41f3ecf39afad2b99b55958dff33b20a
jnewell-7 <jacksonnewell07@gmail.com>
2024-12-04 20:08:33 -0700

    test

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ const knex = require("knex")({
   client: "pg",
   connection: {
     host:
-      process.env.RDS_HOSTNAME ||
-      "awseb-e-it3xmpabbx-stack-awsebrdsdatabase-5vjxonr0zyvk.chiykskmafi4.us-east-1.rds.amazonaws.com",
+      process.env.RDS_HOSTNAME || "awseb-e-it3xmpabbx-stack-awsebrdsdatabase-5vjxonr0zyvk.chiykskmafi4.us-east-1.rds.amazonaws.com",
     user: process.env.RDS_USERNAME || "postgres",
     password: process.env.RDS_PASSWORD || "gocougs123",
     database: process.env.RDS_DB_NAME || "ebdb",
